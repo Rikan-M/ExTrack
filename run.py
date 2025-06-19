11ef11c811724169fdb8eea3ac8e33b4db65a617
Rikan-M <rikanmajimypc10@gmail.com>
2025-06-19 21:01:28 +0530

    Adding runtime.txt

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ with app.app_context():
 
 if __name__=="__main__":
     port=int(os.environ.get("PORT",5000))
-    #host="0.0.0.0",port=port
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0",port=port)
